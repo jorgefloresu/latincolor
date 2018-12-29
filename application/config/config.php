1ed8971a3dd16dcdd05b5f4d6835269273b66e6a
jorgefloresu <jorgefloresu@gmail.com>
2018-12-29 14:45:41 -0600

    cambio de base_url

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost:8888/latincolor/';
+//$config['base_url'] = 'http://localhost:8888/latincolor/';
+$config['base_url'] = 'https://latincolor.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
